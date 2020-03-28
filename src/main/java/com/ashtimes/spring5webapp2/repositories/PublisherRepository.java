cad7ff3fb76ca57c199d07f092b35b1dc72cfca6
Ashish Patel <ashpat86@gmail.com>
2020-03-28 00:18:06 -0400

    Spring5webapp2 Application added Publisher Entity and Publisher Repository

---
+++ src/main/java/com/ashtimes/spring5webapp2/repositories/PublisherRepository.java
@@ -0,0 +1,10 @@
+package com.ashtimes.spring5webapp2.repositories;
+
+import com.ashtimes.spring5webapp2.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+
+
+}
